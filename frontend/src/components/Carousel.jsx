94b7bd1e10b1ef21f65ff995068847f1003d305b
Rohan <rohanmolinillo@gmail.com>
2022-12-06 15:50:19 +0100

    created carousel component

---
+++ frontend/src/components/Carousel.jsx
@@ -0,0 +1,22 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper";
+
+export default function Carousel() {
+  return (
+    <Swiper pagination modules={[Pagination]} className="mySwiper">
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+      <SwiperSlide>Slide 9</SwiperSlide>
+    </Swiper>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,19 @@
+// eslint-disable-next-line import/no-unresolved
+import Carousel from "@components/Carousel";
+
 export default function Home() {
   return (
-    <header className="App-header">
-      <img
-        className="logo"
-        alt="Origins Digital Logo"
-        src="./src/assets/OriginsLogo.png"
-      />
-    </header>
+    <>
+      <header className="App-header">
+        <img
+          className="logo"
+          alt="Origins Digital Logo"
+          src="./src/assets/OriginsLogo.png"
+        />
+      </header>
+      <main>
+        <Carousel />
+      </main>
+    </>
   );
 }
