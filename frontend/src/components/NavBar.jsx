09a8a1d3fe6316ecfedc8969eb3243770ac34c56
Hugo <rodriguez.h@pm.me>
2022-12-06 11:38:28 +0100

    add NavBar component

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+export default function NavBar() {
+  return (
+    <div className="NavBar">
+      <div className="NavBar_links">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
