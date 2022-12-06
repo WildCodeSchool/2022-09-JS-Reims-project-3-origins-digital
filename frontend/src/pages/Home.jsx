93f8dff95a3f8c8c9c1d2a83d4e5b574e9187604
LuciieF <lucie.froissart5917@gmail.com>
2022-12-06 12:07:16 +0100

    main & footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,18 @@
 export default function Home() {
   return (
-    <header className="App-header">
-      <img
-        className="logo"
-        alt="Origins Digital Logo"
-        src="./src/assets/OriginsLogo.png"
-      />
-    </header>
+    <>
+      <header className="App-header">
+        <img
+          className="logo"
+          alt="Origins Digital Logo"
+          src="./src/assets/OriginsLogo.png"
+        />
+      </header>
+      <main>
+        <h1> oui</h1>
+        <p> blablabla</p>
+      </main>
+      <footer>footer</footer>
+    </>
   );
 }
