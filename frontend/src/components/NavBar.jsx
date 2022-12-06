8adb7ed3e310de88acc6f9b2fbaa77cd32a86418
Hugo <rodriguez.h@pm.me>
2022-12-06 15:25:53 +0100

    Full update navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,15 +1,44 @@
+/* eslint-disable react/button-has-type */
+// Pour Romain ou Justine : Si je n'ai pas la ligne pour désactiver le linter, j'ai une erreur eslint qui me dit que le type du bouton doit être défini. Je ne sais pas pourquoi, mais je n'ai pas eu ce problème avec le code de base. Je n'ai pas trouvé de solution pour le moment.
+import React, { useState, useEffect } from "react";
+import "./NavBar.css";
+
 export default function NavBar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [largeur, setLargeur] = useState(window.innerWidth);
+
+  const toggleNavSmallScreen = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setLargeur(window.innerWidth);
+
+      if (window.innerWidth > 500) {
+        setToggleMenu(false);
+      }
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+
   return (
-    <div className="NavBar">
-      <div className="NavBar_links">
-        <ul>
-          <li>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/contact">Contact</a>
-          </li>
+    <nav>
+      {(toggleMenu || largeur > 500) && (
+        <ul className="liste">
+          <li className="items">ÉQUIPE PREMIÈRE</li>
+          <li className="items">FORMATION</li>
+          <li className="items">LÉGENDE</li>
         </ul>
-      </div>
-    </div>
+      )}
+      <button onClick={toggleNavSmallScreen} className="btn">
+        BTN
+      </button>
+    </nav>
   );
 }
