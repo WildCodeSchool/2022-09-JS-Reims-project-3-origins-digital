c732bd40d04fbf4f31b0aebb148c97de6996e0ef
DarkO <romain.bronquard@gmail.com>
2022-12-07 15:31:30 +0100

    add searchbar no usable

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,7 @@
+export default function SearchBar() {
+  return (
+    <div className="SearchBar">
+      <input type="search" id="site-search" placeholder="Search.." />
+    </div>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
+import { BiSearchAlt } from "react-icons/bi";
 import NavBar from "../components/NavBar";
 import Carousel from "../components/Carousel";
+import "./Home.css";
+import SearchBar from "../components/SearchBar";
 
 export default function Home() {
+  const [search, setSearch] = useState(false);
   return (
     <>
       <header className="App-header">
@@ -10,9 +15,19 @@ export default function Home() {
           alt="Origins Digital Logo"
           src="./src/assets/OriginsLogo.png"
         />
+        <NavBar />
+        {search === false && (
+          <button
+            type="submit"
+            className="search"
+            onClick={() => setSearch(!search)}
+          >
+            <BiSearchAlt className="searchIcon" />
+          </button>
+        )}
+        {search === true && <SearchBar />}
       </header>
       <main>
-        <NavBar />
         <h1> Fixtures</h1>
         <p> carousel 1</p>
         <h1> Section 1</h1>
