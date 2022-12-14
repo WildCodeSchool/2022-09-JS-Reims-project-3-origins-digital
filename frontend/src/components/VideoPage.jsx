14452b4bdbc352bdc6c9a7b9e48fa851147f6245
LuciieF <lucie.froissart5917@gmail.com>
2022-12-14 15:59:44 +0100

    beggin of the videopage

--- frontend/src/components/VideoPage.jsx
+++ frontend/src/components/VideoPage.jsx
@@ -1,19 +1,26 @@
+import "./VideoPage.css";
 import React from "react";
 import PropTypes from "prop-types";
-import "./VideoPage.css";
+import { FaLessThan } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function VideoPage({ videotitle, description, category }) {
   return (
     <div>
-      <h1 className="titlevideo">{videotitle}</h1>
-      <h3 className="description">{description}</h3>
+      <Link to="/">
+        <FaLessThan />
+      </Link>
+      <div className="video-informations">
+        <h1 className="titlevideo">{videotitle}</h1>
+        <h3 className="description">{description}</h3>
 
-      <h2 className="subtitle"> Informations</h2>
-      <h3 className="informations"> Catégorie :{category}</h3>
+        <h2 className="subtitle"> INFORMATIONS</h2>
+        <h3 className="informations"> Catégorie :{category}</h3>
 
-      <h2 className="subtitle"> Validité</h2>
+        <h2 className="subtitle"> VALIDITE</h2>
 
-      <h2 className="subtitle"> Langues</h2>
+        <h2 className="subtitle"> LANGUES</h2>
+      </div>
     </div>
   );
 }
