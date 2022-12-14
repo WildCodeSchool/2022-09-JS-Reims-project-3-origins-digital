033ed0ec7c4fd2039b91a733661f041feabf7078
LuciieF <lucie.froissart5917@gmail.com>
2022-12-14 10:38:38 +0100

    structure

---
+++ frontend/src/components/VideoPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./VideoPage.css";
+
+function VideoPage() {
+  return (
+    <div>
+      <h1 className="titlevideo"> Title</h1>
+      <h3 className="description"> Description</h3>
+
+      <h2 className="subtitle"> Informations</h2>
+
+      <h2 className="subtitle"> Validité</h2>
+
+      <h2 className="subtitle"> Langues</h2>
+    </div>
+  );
+}
+
+export default VideoPage;
