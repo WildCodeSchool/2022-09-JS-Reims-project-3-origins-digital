77cc6daf168b7f81e80b20bb0b8a6da80b38d65a
Yhugzee <rodriguez.h@pm.me>
2022-12-15 17:05:47 +0100

    add video on videopage

--- frontend/src/components/VideoPage.jsx
+++ frontend/src/components/VideoPage.jsx
@@ -2,6 +2,7 @@ import "./VideoPage.css";
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { FaLessThan } from "react-icons/fa";
+import NavBar from "./NavBar";
 
 export default function VideoPage() {
   const { id } = useParams();
@@ -12,10 +13,19 @@ export default function VideoPage() {
       <Link to="/">
         <FaLessThan />
       </Link>
+      <NavBar />
       <div className="video-informations">
+        <iframe
+          width="360"
+          height="250"
+          src="https://www.youtube.com/embed/mBiE_g074_Q"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
         <h1 className="titlevideo">{/* title */}</h1>
         <h3 className="description">{/* description */}</h3>
-
         <h2 className="subtitle"> INFORMATIONS</h2>
         <h3 className="informations"> Catégorie :{/* category */}</h3>
 
