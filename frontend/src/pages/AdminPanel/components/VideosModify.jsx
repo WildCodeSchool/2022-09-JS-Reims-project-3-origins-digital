3186af2006d9ef07b2ad613c01289a6ae7189bf2
Yhugzee <rodriguez.h@pm.me>
2023-01-18 11:37:57 +0100

    show component AdminMenu

--- frontend/src/pages/AdminPanel/components/VideosModify.jsx
+++ frontend/src/pages/AdminPanel/components/VideosModify.jsx
@@ -1,8 +1,10 @@
 import "./VideosModify.css";
+import AdminMenu from "./AdminMenu";
 
 export default function VideosModify() {
   return (
     <div>
+      <AdminMenu />
       <h1 className="title-vm">Modifier une vidéo</h1>
     </div>
   );
