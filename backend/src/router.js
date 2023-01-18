e7b904cbdf800b085ee37178a06df89c88ce080a
Charlotte Kieffer <kindred242@gmail.Com>
2023-01-18 11:48:16 +0100

    created the routes in backend for the admin panel

--- backend/src/router.js
+++ backend/src/router.js
@@ -22,15 +22,15 @@ router.get("/category", categoryControllers.getCategory);
 router.get("/category/:id", categoryControllers.getOneCategory);
 router.use(verifyToken);
 
-router.put("/videos/:id", videoControllers.editVideo);
-router.post("/videos", videoControllers.addVideo);
-router.delete("/videos/:id", videoControllers.destroyVideo);
-router.put("/category/:id", categoryControllers.editCategory);
-router.post("/category", categoryControllers.addCategory);
-router.delete("/category/:id", categoryControllers.destroyCategory);
+router.put("admin/videos/modify/:id", videoControllers.editVideo);
+router.post("admin/videos/add", videoControllers.addVideo);
+router.delete("admin/videos/modify/:id", videoControllers.destroyVideo);
+router.put("admin/category/:id", categoryControllers.editCategory);
+router.post("admin/category", categoryControllers.addCategory);
+router.delete("admin/category/:id", categoryControllers.destroyCategory);
 
-router.put("/users/:id", hashPassword, userControllers.editUser);
+router.put("admin/users/:id", hashPassword, userControllers.editUser);
 
-router.delete("/users/:id", userControllers.destroyUser);
+router.delete("admin/users/:id", userControllers.destroyUser);
 
 module.exports = router;
