be77418f7d7fec8ed12f58c9dc9709f271b4325f
Romain Bronquard <romain.bronquard@gmail.com>
2023-01-18 14:45:51 +0100

    add context in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,23 +8,26 @@ import "./App.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import { VideoContextProvider } from "./contexts/ContextVideos";
+import { CategoryContextProvider } from "./contexts/ContextCategory";
 
 function App() {
   return (
-    <VideoContextProvider>
-      <Router>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="search" element={<SearchBar />} />
-            <Route path="/videos/:id" element={<VideoPage />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/admin" element={<AdminPanel />} />
-          </Routes>
-        </div>
-      </Router>
-    </VideoContextProvider>
+    <CategoryContextProvider>
+      <VideoContextProvider>
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="search" element={<SearchBar />} />
+              <Route path="/videos/:id" element={<VideoPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/admin" element={<AdminPanel />} />
+            </Routes>
+          </div>
+        </Router>
+      </VideoContextProvider>
+    </CategoryContextProvider>
   );
 }
 
