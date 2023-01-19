80b001f8e6a08c88c8adfb7fc3bd11b3cd6e8b4a
Rohan <rohanmolinillo@gmail.com>
2023-01-19 13:46:02 +0100

    css on navbar and js for hide navbar on scroll

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -10,6 +10,7 @@ const breakpoint = 992;
 export default function NavBar() {
   const [toggleMenu, setToggleMenu] = useState(false);
   const [width, setWidth] = useState(window.innerWidth);
+  const [show, setShow] = useState(true);
 
   const toggleNavSmallScreen = () => {
     setToggleMenu(!toggleMenu);
@@ -26,47 +27,66 @@ export default function NavBar() {
 
     window.addEventListener("resize", changeWidth);
 
+    let oldValue = 0;
+    let newValue = 0;
+
+    window.addEventListener("scroll", () => {
+      // Get the new Value
+      newValue = window.scrollY;
+
+      if (oldValue - newValue < 0) {
+        setShow(false);
+      } else if (oldValue - newValue > 0) {
+        setShow(true);
+      }
+
+      // Update the old value
+      oldValue = newValue;
+    });
+
     return () => {
       window.removeEventListener("resize", changeWidth);
     };
   }, []);
 
   return (
-    <nav className="nav-bar">
-      {toggleMenu === false ? (
-        <button type="button" onClick={toggleNavSmallScreen} className="btn">
-          <RxHamburgerMenu className="menuIcon" />
-        </button>
-      ) : (
-        <button type="button" onClick={toggleNavSmallScreen} className="btn">
-          <RxCross1 className="menuIcon" />
-        </button>
-      )}
-      <img
-        className="logo"
-        alt="Origins Digital Logo"
-        src="./src/assets/OriginsLogo.png"
-      />
-      {(toggleMenu || width > breakpoint) && (
-        <ul className="liste">
-          <li className="items">ÉQUIPE PREMIÈRE</li>
-          <li className="items">FORMATION</li>
-          <li className="items">LÉGENDE</li>
-          <li className="items">
-            <Link style={{ textDecoration: "none" }} to="/admin">
-              ADMIN
-            </Link>
-          </li>
-        </ul>
-      )}
-      <div className="button-right">
-        <Link to="/login">
-          <CgProfile className="profile" />
-        </Link>
-        <Link to="/search">
-          <BiSearchAlt className="searchIcon" />
-        </Link>
-      </div>
-    </nav>
+    <div className="nav-bar-box">
+      <nav className="nav-bar" style={show ? { top: 0 } : { top: -150 }}>
+        {toggleMenu === false ? (
+          <button type="button" onClick={toggleNavSmallScreen} className="btn">
+            <RxHamburgerMenu className="menuIcon" />
+          </button>
+        ) : (
+          <button type="button" onClick={toggleNavSmallScreen} className="btn">
+            <RxCross1 className="menuIcon" />
+          </button>
+        )}
+        <img
+          className="logo"
+          alt="Origins Digital Logo"
+          src="./src/assets/OriginsLogo.png"
+        />
+        {(toggleMenu || width > breakpoint) && (
+          <ul className="liste">
+            <li className="items">ÉQUIPE PREMIÈRE</li>
+            <li className="items">FORMATION</li>
+            <li className="items">LÉGENDE</li>
+            <li className="items">
+              <Link style={{ textDecoration: "none" }} to="/admin">
+                ADMIN
+              </Link>
+            </li>
+          </ul>
+        )}
+        <div className="button-right">
+          <Link to="/login">
+            <CgProfile className="profile" />
+          </Link>
+          <Link to="/search">
+            <BiSearchAlt className="searchIcon" />
+          </Link>
+        </div>
+      </nav>
+    </div>
   );
 }
