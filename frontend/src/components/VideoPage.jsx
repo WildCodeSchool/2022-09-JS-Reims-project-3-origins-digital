a36809751665d0d6696e0021b196bbd5a4cdadec
Lucie <99013579+LuciieF@users.noreply.github.com>
2023-01-26 16:13:09 +0100

    new div video_container and css

--- frontend/src/components/VideoPage.jsx
+++ frontend/src/components/VideoPage.jsx
@@ -22,38 +22,40 @@ export default function VideoPage() {
         <Link to="/">
           <FaLessThan className="return-button" />
         </Link>
-        <div className="video">
-          <div className="video-player">
-            <iframe
-              className="responsive-video"
-              src={video.link}
-              title={video.title}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
-          </div>
-          <div className="video-informations">
-            <h1 className="titlevideo">{video.title}</h1>
-            <h3 className="description">{video.description_video}</h3>
-            <div className="hashtag">
-              <p className="hash">#Football</p>
-              <p className="hash">#Mondial</p>
-              <p className="hash">#Griezmann</p>
+        <div className="video_container">
+          <div className="video">
+            <div className="video-player">
+              <iframe
+                className="responsive-video"
+                src={video.link}
+                title={video.title}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
             </div>
-            <hr className="barre" />
-            <h2 className="informations"> INFORMATIONS</h2>
-            <h3 className="category">
-              {" "}
-              Catégorie : {categoryVideo.category_name}
-            </h3>
-            <h2 className="validite"> VALIDITE : </h2>
-            <h3 className="validateInfosStart">
-              Publié : {video.publication_date}
-            </h3>
-            <h3 className="validateInfosEnd"> </h3>
-            <h2 className="subtitle"> LANGUES</h2>
-            <h3 className="subtitleInfos">French, English, Spanish</h3>
+            <div className="video-informations">
+              <h1 className="titlevideo">{video.title}</h1>
+              <h3 className="description">{video.description_video}</h3>
+              <div className="hashtag">
+                <p className="hash">#Football</p>
+                <p className="hash">#Mondial</p>
+                <p className="hash">#Griezmann</p>
+              </div>
+              <hr className="barre" />
+              <h2 className="informations"> INFORMATIONS</h2>
+              <h3 className="category">
+                {" "}
+                Catégorie : {categoryVideo.category_name}
+              </h3>
+              <h2 className="validite"> VALIDITE : </h2>
+              <h3 className="validateInfosStart">
+                Publié : {video.publication_date}
+              </h3>
+              <h3 className="validateInfosEnd"> </h3>
+              <h2 className="subtitle"> LANGUES</h2>
+              <h3 className="subtitleInfos">French, English, Spanish</h3>
+            </div>
           </div>
         </div>
       </div>
